0d403e7b15f19586f432b4f04cc57121f753d215
Antoon Lievens <antoonlievens@gmail.com>
2021-09-24 11:15:34 +0200

    Update to new format
    now handles native read.QX output

--- MonoColor ddPlot.r
+++ MonoColor ddPlot.r
@@ -1,4 +1,9 @@
-ddplot <- function(drp, ...){
-      drp <- na.omit(drp)
-      temp <- sample(c(1:length(drp)), size = length(drp), replace = F)
-      plot(drp ~ temp, col = rgb(123, 168, 194, maxColorValue = 255), bty = "L", pch = 20 , xlab = "observation", ylab = "FU", ylim = c(min(drp), max(drp)), ...)}
+ddplot <- function(drp, well = 1, channel = 1,...){
+      drp1 <- if(channel == 1){na.omit(drp$Ch1[, well])}else{na.omit(drp$Ch2[, well])}
+      temp <- sample(c(1:length(drp1)), size = length(drp1), replace = F)
+      plot(drp1 ~ temp, col = rgb(123, 168, 194, maxColorValue = 255), bty = "L", pch = '.' , cex = 5, xlab = "observation", ylab = "FU", ...)}
+
+tdplot <- function(drp, well = 1,...){
+      drp1 <- na.omit(drp$Ch1[, well])
+      drp2 <- na.omit(drp$Ch2[, well])
+      plot(drp1 ~ drp2, col = rgb(123, 168, 194, maxColorValue = 255), bty = "L", pch = '.' , cex = 5, xlab = "FU_Ch2", ylab = "FU_Ch1", ...)}
